5b30db37c7bcb34f6ba5c0c004757c4758e75f81
babubrjf <116585493+babubrjf@users.noreply.github.com>
2026-09-03 07:32:45 -0300

    fix: adiciona casos de teste

--- energiaEletrica/src/test/java/FaturaTest.java
+++ energiaEletrica/src/test/java/FaturaTest.java
@@ -6,7 +6,7 @@ class FaturaTest {
 
 	// residencial
 	@Test
-	void deveRetornarTarifaSocialAbaixoDe100() {
+	void deveRetornarTarifaSocial() {
 		Fatura fatura = new Fatura();
 		fatura.setConsumoKwh(99.9);
 		fatura.setTipoCliente("residencial");
@@ -16,51 +16,87 @@ class FaturaTest {
 	}
 
 	@Test
-	void deveRetornarTarifaNormalEm100() {
+	void deveRetornarTarifaNormalMinima() {
 		Fatura fatura = new Fatura(100, "residencial");
 		assertEquals("tarifa normal", fatura.calcularTarifa());
 	}
 
 	@Test
-	void deveRetornarTarifaAltaEm300() {
+	void deveRetornarTarifaNormalMaxima() {
+		Fatura fatura = new Fatura(299.9, "residencial");
+		assertEquals("tarifa normal", fatura.calcularTarifa());
+	}
+
+	@Test
+	void deveRetornarTarifaAltaMinima() {
 		Fatura fatura = new Fatura(300, "residencial");
 		assertEquals("tarifa alta", fatura.calcularTarifa());
 	}
 
 	@Test
-	void deveRetornarTarifaMuitoAltaEm500() {
+	void deveRetornarTarifaAltaMaxima() {
+		Fatura fatura = new Fatura(499.9, "residencial");
+		assertEquals("tarifa alta", fatura.calcularTarifa());
+	}
+
+	@Test
+	void deveRetornarTarifaMuitoAltaMinima() {
 		Fatura fatura = new Fatura(500, "residencial");
 		assertEquals("tarifa muito alta", fatura.calcularTarifa());
 	}
 
 	@Test
-	void deveRetornarTarifaDeExcedenteEm800() {
+	void deveRetornarTarifaMuitoAltaMaxima() {
+		Fatura fatura = new Fatura(799.9, "residencial");
+		assertEquals("tarifa muito alta", fatura.calcularTarifa());
+	}
+
+	@Test
+	void deveRetornarTarifaDeExcedente() {
 		Fatura fatura = new Fatura(800, "residencial");
 		assertEquals("tarifa de excedente", fatura.calcularTarifa());
 	}
 
 	// comercial
 	@Test
-	void deveRetornarTarifaBasicaAbaixoDe500() {
+	void deveRetornarTarifaBasica() {
 		Fatura fatura = new Fatura(499.9, "comercial");
 		assertEquals("tarifa básica", fatura.calcularTarifa());
 	}
 
 	@Test
-	void deveRetornarTarifaIntermediariaEm500() {
+	void deveRetornarTarifaIntermediariaMinima() {
 		Fatura fatura = new Fatura(500, "comercial");
 		assertEquals("tarifa intermediária", fatura.calcularTarifa());
 	}
 
 	@Test
-	void deveRetornarTarifaAvancadaEm1500() {
+	void deveRetornarTarifaIntermediariaMaxima() {
+		Fatura fatura = new Fatura(1499.9, "comercial");
+		assertEquals("tarifa intermediária", fatura.calcularTarifa());
+	}
+
+	@Test
+	void deveRetornarTarifaAvancadaMinima() {
 		Fatura fatura = new Fatura(1500, "comercial");
 		assertEquals("tarifa avançada", fatura.calcularTarifa());
 	}
 
 	@Test
-	void deveRetornarTarifaIndustrialEm3000() {
+	void deveRetornarTarifaAvancadaMaxima() {
+		Fatura fatura = new Fatura(2999.9, "comercial");
+		assertEquals("tarifa avançada", fatura.calcularTarifa());
+	}
+
+	@Test
+	void deveRetornarTarifaIndustrialMinima() {
 		Fatura fatura = new Fatura(3000, "comercial");
+		assertEquals("tarifa industrial", fatura.calcularTarifa());
+	}
+
+	@Test
+	void deveRetornarTarifaIndustrialMaxima() {
+		Fatura fatura = new Fatura(5999.9, "comercial");
 		assertEquals("tarifa industrial", fatura.calcularTarifa());
 	}
 
